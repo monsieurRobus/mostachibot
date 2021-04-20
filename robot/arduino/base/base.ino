f51f293a3d49230b307414aa6566e69f6aaf0659
soyMon <cdperez.85@gmail.com>
2021-04-20 23:50:19 +0200

    Añadidos ojos neopixel, pantalla expresión LCD, primeros pasos en implementar movimientos OTTO

--- robot/arduino/base/base.ino
+++ robot/arduino/base/base.ino
@@ -1,6 +1,34 @@
-                      #include <MD_MAX72xx.h>
+
+
+#include <Wire.h>
+#include <splash.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_GrayOLED.h>
+#include <Adafruit_SPITFT.h>
+#include <Adafruit_SPITFT_Macros.h>
+#include <gfxfont.h>
+#include <MD_MAX72xx.h>
 #include <SPI.h>
 #include <Servo.h>
+
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+ #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#endif
+
+#define NUMPIXELS 12
+#define PINIZQ 6
+#define PINDER 7
+
+
+Adafruit_SSD1306 oled(128,64,&Wire,4);
+Adafruit_NeoPixel ojoizq(NUMPIXELS, PINIZQ, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel ojoder(NUMPIXELS, PINDER, NEO_GRB + NEO_KHZ800);
+
+
+
+/* PROTOCOLO COMUNICACION */
 
 
 
@@ -13,6 +41,8 @@ int pinCS = 3;
 int pinCLK = 4;
 //Inicializamos la pantalla de leds MAX7219 en modo generico, en el pin CS indicado y el numero de pantallas 
 MD_MAX72XX pLED = MD_MAX72XX(MD_MAX72XX::DR0CR0RR1_HW, pinDIN,pinCLK,pinCS, 1);
+
+
 
 
 //Funciones y movimientos
@@ -100,16 +130,26 @@ void estrella(){
   pLED.setColumn(1,B10010100);
   pLED.setColumn(0,B11101100);
   delay(100);
+  mensaje="";
 }
+
+
 
 void setup() {
   //Inicializamos el puerto serie a determinada velocidad  
   Serial.begin(baud);
+  ojoizq.begin();
+  ojoder.begin();
   pLED.begin();
   setupLeds();
-
-   //Realizamos breve test de comprobación de los leds
-   
+  Wire.begin();
+  oled.begin(SSD1306_SWITCHCAPVCC,0x3C);
+  ojoizq.fill(ojoizq.Color(20,20,20,2),0,12);
+  ojoder.fill(ojoder.Color(50,50,50,50),0,12);
+  ojoizq.show();
+  ojoder.show();
+  
+ 
  
    
  
@@ -117,15 +157,24 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
- 
+  oled.clearDisplay();
+  oled.setTextColor(WHITE);
+  oled.setCursor(40,0);
+  oled.setTextSize(7);
+  oled.print("w");
+  oled.display();
 
   if(Serial.available()>0)
   {
   
        
     mensaje = Serial.readString();
-    if(mensaje == "encender"){ 
+    
+    if(mensaje == "encender\n"){ 
+      
      estrella();
+     Serial.println("Retro calvo <3");     
+    
     }
     if(mensaje == "apagar")
     {
@@ -135,4 +184,32 @@ void loop() {
     
     
   }
-}
+  else
+  {
+
+    // Si no hay mensaje en el serial, estamos en IDLE
+
+    for (int i=20;i<100;i++)
+    {      
+    ojoizq.fill(ojoizq.Color(i,i,i,i),0,12);
+    ojoder.fill(ojoder.Color(i,i,i,i),0,12);
+    delay(6);
+    ojoizq.show();
+    ojoder.show();
+    }
+  
+    for (int i=100;i>20;i--)
+    {      
+    ojoizq.fill(ojoizq.Color(i,i,i,i),0,12);
+    ojoder.fill(ojoder.Color(i,i,i,i),0,12);
+    delay(6);
+    ojoizq.show();
+    ojoder.show();
+    }
+
+  
+  
+    
+  }
+  
+  }
